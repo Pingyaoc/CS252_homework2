857a48ca81f0acf991222b46770ed6a6e6bc313e
Pingyao.Chen <Pingyaoc@users.noreply.github.com>
2015-05-17 19:51:14 +0800

    20150517畫方形
    由user輸入長寬然後畫出方形

---
+++ DrawSquare.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS252_homework2
+{
+    internal class DrawSquare
+    {
+        private int _length;
+        public Bitmap b;
+        public Pen p;
+
+        public int Length
+        {
+            get { return this._length; }
+            set
+            {
+                this._length = value;
+                if (value > 5)
+                {
+                    this._length = 5;
+                    MessageBox.Show("Maximum length is 5");
+                }
+                else if (value <= 0)
+                    this._length = 1;
+                else
+                    this._length = value;
+            }
+        }
+
+        private int _width;
+
+        public int Width
+        {
+            get { return this._width; }
+            set
+            {
+                this._width = value;
+                if (value > 5)
+                {
+                    this._width = 5;
+                    MessageBox.Show("Maximum width is 5");
+                }
+                else if (value <= 0)
+                    this._width = 1;
+                else
+                    this._width = value;
+            }
+        }
+
+        private void line(Graphics g, Pen p, int x1, int y1, int x2, int y2)
+        {
+            g.DrawLine(p, x1, y1, x2, y2); // NEW 畫線座標
+        }
+
+        public int UserDrawSuare(int _length, int _width, int _pBoxWidth, int _pBoxHigh)
+        {
+            b = new Bitmap(_pBoxWidth, _pBoxHigh);
+            p = new Pen(Color.Black, 3);
+
+            line(Graphics.FromImage(b), p, 50, 0, _width * 50, 0);
+
+            line(Graphics.FromImage(b), p, 50, 0, 50, _length * 50);
+            line(Graphics.FromImage(b), p, _width * 50, 0, _width * 50, _length * 50);
+
+            line(Graphics.FromImage(b), p, 50, _length * 50, _width * 50, _length * 50);
+            return 0;
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CS252_homework2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SquareLengthLabel1 = new System.Windows.Forms.Label();
+            this.SquareWidthLabel2 = new System.Windows.Forms.Label();
+            this.SquareLengthTextBox1 = new System.Windows.Forms.TextBox();
+            this.SquareWidthTextBox2 = new System.Windows.Forms.TextBox();
+            this.DrawSquareButton1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SquareLengthLabel1
+            // 
+            this.SquareLengthLabel1.AutoSize = true;
+            this.SquareLengthLabel1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.SquareLengthLabel1.Location = new System.Drawing.Point(24, 25);
+            this.SquareLengthLabel1.Name = "SquareLengthLabel1";
+            this.SquareLengthLabel1.Size = new System.Drawing.Size(122, 20);
+            this.SquareLengthLabel1.TabIndex = 0;
+            this.SquareLengthLabel1.Text = "Sauqre_Length";
+            // 
+            // SquareWidthLabel2
+            // 
+            this.SquareWidthLabel2.AutoSize = true;
+            this.SquareWidthLabel2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.SquareWidthLabel2.Location = new System.Drawing.Point(26, 64);
+            this.SquareWidthLabel2.Name = "SquareWidthLabel2";
+            this.SquareWidthLabel2.Size = new System.Drawing.Size(115, 20);
+            this.SquareWidthLabel2.TabIndex = 1;
+            this.SquareWidthLabel2.Text = "Square_Width";
+            // 
+            // SquareLengthTextBox1
+            // 
+            this.SquareLengthTextBox1.Location = new System.Drawing.Point(214, 25);
+            this.SquareLengthTextBox1.Name = "SquareLengthTextBox1";
+            this.SquareLengthTextBox1.Size = new System.Drawing.Size(100, 22);
+            this.SquareLengthTextBox1.TabIndex = 2;
+            // 
+            // SquareWidthTextBox2
+            // 
+            this.SquareWidthTextBox2.Location = new System.Drawing.Point(214, 61);
+            this.SquareWidthTextBox2.Name = "SquareWidthTextBox2";
+            this.SquareWidthTextBox2.Size = new System.Drawing.Size(100, 22);
+            this.SquareWidthTextBox2.TabIndex = 3;
+            // 
+            // DrawSquareButton1
+            // 
+            this.DrawSquareButton1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.DrawSquareButton1.Location = new System.Drawing.Point(384, 21);
+            this.DrawSquareButton1.Name = "DrawSquareButton1";
+            this.DrawSquareButton1.Size = new System.Drawing.Size(137, 47);
+            this.DrawSquareButton1.TabIndex = 4;
+            this.DrawSquareButton1.Text = "StartDarwSqure";
+            this.DrawSquareButton1.UseVisualStyleBackColor = true;
+            this.DrawSquareButton1.Click += new System.EventHandler(this.DrawSquareButton1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(5, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(572, 264);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 355);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.DrawSquareButton1);
+            this.Controls.Add(this.SquareWidthTextBox2);
+            this.Controls.Add(this.SquareLengthTextBox1);
+            this.Controls.Add(this.SquareWidthLabel2);
+            this.Controls.Add(this.SquareLengthLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label SquareLengthLabel1;
+        private System.Windows.Forms.Label SquareWidthLabel2;
+        private System.Windows.Forms.TextBox SquareLengthTextBox1;
+        private System.Windows.Forms.TextBox SquareWidthTextBox2;
+        private System.Windows.Forms.Button DrawSquareButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
